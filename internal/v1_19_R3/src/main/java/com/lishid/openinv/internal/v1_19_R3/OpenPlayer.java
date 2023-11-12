bc7e13f245174088cac4a35515b27f5a45d02e6a
Adam <Jikoo.Games@gmail.com>
2023-11-12 12:34:32 -0500

    Expand preserved old data (#166)
    * Fix session timestamps being erroneously updated
    * Fix vehicle possibly not being re-mounted

--- internal/v1_19_R3/src/main/java/com/lishid/openinv/internal/v1_19_R3/OpenPlayer.java
+++ internal/v1_19_R3/src/main/java/com/lishid/openinv/internal/v1_19_R3/OpenPlayer.java
@@ -16,15 +16,21 @@
 
 package com.lishid.openinv.internal.v1_19_R3;
 
-import java.io.File;
 import net.minecraft.Util;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.NumericTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.storage.PlayerDataStorage;
 import org.apache.logging.log4j.LogManager;
 import org.bukkit.craftbukkit.v1_19_R3.CraftServer;
 import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
 
 public class OpenPlayer extends CraftPlayer {
 
@@ -52,13 +58,9 @@ public class OpenPlayer extends CraftPlayer {
             setExtraData(playerData);
 
             if (!isOnline()) {
-                // Special case: save old vehicle data
+                // Preserve certain data when offline.
                 CompoundTag oldData = worldNBTStorage.load(player);
-
-                if (oldData != null && oldData.contains("RootVehicle", 10)) {
-                    // See net.minecraft.server.PlayerList#a(NetworkManager, EntityPlayer) and net.minecraft.server.EntityPlayer#b(NBTTagCompound)
-                    playerData.put("RootVehicle", oldData.getCompound("RootVehicle"));
-                }
+                revertSpecialValues(playerData, oldData);
             }
 
             File file = File.createTempFile(player.getStringUUID() + "-", ".dat", worldNBTStorage.getPlayerDir());
@@ -68,6 +70,73 @@ public class OpenPlayer extends CraftPlayer {
             Util.safeReplaceFile(file1, file, file2);
         } catch (Exception e) {
             LogManager.getLogger().warn("Failed to save player data for {}: {}", player.getScoreboardName(), e);
+        }
+    }
+
+    private void revertSpecialValues(@NotNull CompoundTag newData, @Nullable CompoundTag oldData) {
+        if (oldData == null) {
+            return;
+        }
+
+        // Prevent vehicle deletion.
+        if (oldData.contains("RootVehicle", Tag.TAG_COMPOUND)) {
+            // See net.minecraft.server.players.PlayerList#save(ServerPlayer)
+            // See net.minecraft.server.level.ServerPlayer#addAdditionalSaveData(CompoundTag)
+            try {
+                Tag attach = oldData.get("Attach");
+                if (attach != null) {
+                    newData.putUUID("Attach", NbtUtils.loadUUID(attach));
+                }
+            } catch (IllegalArgumentException ignored) {
+                // Likely will not re-mount successfully, but at least the mount will not be deleted.
+            }
+            newData.put("Entity", oldData.getCompound("Entity"));
+            newData.put("RootVehicle", oldData.getCompound("RootVehicle"));
+        }
+
+        // Revert automatic updates to play timestamps.
+        copyValue(oldData, newData, "bukkit", "lastPlayed", NumericTag.class);
+        copyValue(oldData, newData, "Paper", "LastSeen", NumericTag.class);
+        copyValue(oldData, newData, "Paper", "LastLogin", NumericTag.class);
+    }
+
+    private <T extends Tag> void copyValue(
+        @NotNull CompoundTag source,
+        @NotNull CompoundTag target,
+        @NotNull String container,
+        @NotNull String key,
+        @NotNull Class<T> tagType) {
+        CompoundTag oldContainer = getTag(source, container, CompoundTag.class);
+        CompoundTag newContainer = getTag(target, container, CompoundTag.class);
+
+        // Container being null means the server implementation doesn't store this data.
+        if (oldContainer == null || newContainer == null) {
+            return;
+        }
+
+        // If old tag exists, copy it to new location, removing otherwise.
+        setTag(newContainer, key, getTag(oldContainer, key, tagType));
+    }
+
+    private <T extends Tag> @Nullable T getTag(
+        @NotNull CompoundTag container,
+        @NotNull String key,
+        @NotNull Class<T> dataType) {
+        Tag value = container.get(key);
+        if (value == null || !dataType.isAssignableFrom(value.getClass())) {
+            return null;
+        }
+        return dataType.cast(value);
+    }
+
+    private <T extends Tag> void setTag(
+        @NotNull CompoundTag container,
+        @NotNull String key,
+        @Nullable T data) {
+        if (data == null) {
+            container.remove(key);
+        } else {
+            container.put(key, data);
         }
     }
 
--- internal/v1_20_R2/src/main/java/com/lishid/openinv/internal/v1_20_R2/OpenPlayer.java
+++ internal/v1_20_R2/src/main/java/com/lishid/openinv/internal/v1_20_R2/OpenPlayer.java
@@ -17,14 +17,20 @@
 package com.lishid.openinv.internal.v1_20_R2;
 
 import com.mojang.logging.LogUtils;
-import java.io.File;
 import net.minecraft.Util;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.NumericTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.storage.PlayerDataStorage;
 import org.bukkit.craftbukkit.v1_20_R2.CraftServer;
 import org.bukkit.craftbukkit.v1_20_R2.entity.CraftPlayer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
 
 public class OpenPlayer extends CraftPlayer {
 
@@ -48,13 +54,9 @@ public class OpenPlayer extends CraftPlayer {
             setExtraData(playerData);
 
             if (!isOnline()) {
-                // Special case: save old vehicle data
+                // Preserve certain data when offline.
                 CompoundTag oldData = worldNBTStorage.load(player);
-
-                if (oldData != null && oldData.contains("RootVehicle", 10)) {
-                    // See net.minecraft.server.PlayerList#a(NetworkManager, EntityPlayer) and net.minecraft.server.EntityPlayer#b(NBTTagCompound)
-                    playerData.put("RootVehicle", oldData.getCompound("RootVehicle"));
-                }
+                revertSpecialValues(playerData, oldData);
             }
 
             File file = File.createTempFile(player.getStringUUID() + "-", ".dat", worldNBTStorage.getPlayerDir());
@@ -64,6 +66,73 @@ public class OpenPlayer extends CraftPlayer {
             Util.safeReplaceFile(file1, file, file2);
         } catch (Exception e) {
             LogUtils.getLogger().warn("Failed to save player data for {}: {}", player.getScoreboardName(), e);
+        }
+    }
+
+    private void revertSpecialValues(@NotNull CompoundTag newData, @Nullable CompoundTag oldData) {
+        if (oldData == null) {
+            return;
+        }
+
+        // Prevent vehicle deletion.
+        if (oldData.contains("RootVehicle", Tag.TAG_COMPOUND)) {
+            // See net.minecraft.server.players.PlayerList#save(ServerPlayer)
+            // See net.minecraft.server.level.ServerPlayer#addAdditionalSaveData(CompoundTag)
+            try {
+                Tag attach = oldData.get("Attach");
+                if (attach != null) {
+                    newData.putUUID("Attach", NbtUtils.loadUUID(attach));
+                }
+            } catch (IllegalArgumentException ignored) {
+                // Likely will not re-mount successfully, but at least the mount will not be deleted.
+            }
+            newData.put("Entity", oldData.getCompound("Entity"));
+            newData.put("RootVehicle", oldData.getCompound("RootVehicle"));
+        }
+
+        // Revert automatic updates to play timestamps.
+        copyValue(oldData, newData, "bukkit", "lastPlayed", NumericTag.class);
+        copyValue(oldData, newData, "Paper", "LastSeen", NumericTag.class);
+        copyValue(oldData, newData, "Paper", "LastLogin", NumericTag.class);
+    }
+
+    private <T extends Tag> void copyValue(
+        @NotNull CompoundTag source,
+        @NotNull CompoundTag target,
+        @NotNull String container,
+        @NotNull String key,
+        @NotNull Class<T> tagType) {
+        CompoundTag oldContainer = getTag(source, container, CompoundTag.class);
+        CompoundTag newContainer = getTag(target, container, CompoundTag.class);
+
+        // Container being null means the server implementation doesn't store this data.
+        if (oldContainer == null || newContainer == null) {
+            return;
+        }
+
+        // If old tag exists, copy it to new location, removing otherwise.
+        setTag(newContainer, key, getTag(oldContainer, key, tagType));
+    }
+
+    private <T extends Tag> @Nullable T getTag(
+        @NotNull CompoundTag container,
+        @NotNull String key,
+        @NotNull Class<T> dataType) {
+        Tag value = container.get(key);
+        if (value == null || !dataType.isAssignableFrom(value.getClass())) {
+            return null;
+        }
+        return dataType.cast(value);
+    }
+
+    private <T extends Tag> void setTag(
+        @NotNull CompoundTag container,
+        @NotNull String key,
+        @Nullable T data) {
+        if (data == null) {
+            container.remove(key);
+        } else {
+            container.put(key, data);
         }
     }
 
